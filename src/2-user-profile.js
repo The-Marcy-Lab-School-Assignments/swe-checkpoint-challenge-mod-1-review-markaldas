a0df6fdc593cd2d317460e3ab423c318962a1f93
Mark <markaldas41@gmail.com>
2025-12-11 11:40:43 -0500

    completed everthing :)

--- src/2-user-profile.js
+++ src/2-user-profile.js
@@ -1,26 +1,35 @@
 // Return a new user object with username, email, isActive: true, loginCount: 0
 const createUser = (username, email) => {
-
+  const newUser = {
+    username: username,
+    email: email,
+    isActive: true,
+    loginCount: 0
+  }
+  return newUser;
 };
 
 // Increase the user's loginCount by 1
 const incrementLogin = (user) => {
-
+  return user.loginCount += 1
 };
 
 // Set isActive to false and delete the email property. Return the user.
 const deactivateUser = (user) => {
-
+  user.isActive = false;
+  delete user.email;
+  return user;
 };
 
 // Print each property and value in the format "key: value"
 const printUserInfo = (user) => {
-
+  const keyValue = Object.keys(user).forEach((key) => console.log(`${key}: ${user[key]}`))
+  return keyValue
 };
 
 // BONUS: Return a true copy of the user object (not a reference)
 const cloneUser = (user) => {
-
+  return { ...user }
 };
 
 module.exports = {
